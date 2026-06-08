3921bb844a0bbe374fc7cc286b0629a8d42fcbb5
Devin Fromond <74107804+Dfromond@users.noreply.github.com>
2026-06-08 19:03:48 -0400

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String args[]) {
-        String s = "Veiled Caverns: 1234, 56";
-        String[] arrOfInfo = s.split(", |\\: ");
-        for(int i = 0; i < arrOfInfo.length; i++) {
-        System.out.println("\"" + arrOfInfo[i] + "\"");
-        }
-    }
-}
